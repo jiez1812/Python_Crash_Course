1a5d9761aaa7793781e19a1a71dc7e10a23390a4
Jiez <jieztee@gmail.com>
2017-09-14 21:53:13 +0800

    Slicing a List - players.py

---
+++ CH4_WORKING_WITH_LISTS/players.py
@@ -0,0 +1,12 @@
+# Slicing a List
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# Print frist three players
+print(players[0:3])
+#Slice at index 1 and end at index 4
+print(players[1:4])
+# Slice at the beginning of the list
+print(players[:4])
+# Python return all items from the third item through the end of the list
+print(players[2:])
+# Print last three players would continue to work as the list of players changes in size
+print(players[-3:])
