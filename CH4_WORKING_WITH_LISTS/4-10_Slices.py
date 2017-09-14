575bf7b959c6ab2fb4467ea540dfcda05d279838
Jiez <jieztee@gmail.com>
2017-09-14 22:31:01 +0800

    4-10_Slices

---
+++ CH4_WORKING_WITH_LISTS/4-10_Slices.py
@@ -0,0 +1,7 @@
+magicians = ["alice", "david", "carolina", "madoka", "regis"]
+print("The first three magicians in the list are:")
+print(magicians[:3])
+print("\nThree magicians from the iddle of the list are:")
+print(magicians[1:3])
+print("\nThe last three items in the list are:")
+print(magicians[-3:])
