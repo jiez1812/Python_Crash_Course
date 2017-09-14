fa2fd73e2426f5500a025043701c83442e6718a5
Jiez <jieztee@gmail.com>
2017-09-14 22:12:55 +0800

    Looping through a Slice - players.py

--- CH4_WORKING_WITH_LISTS/players.py
+++ CH4_WORKING_WITH_LISTS/players.py
@@ -10,3 +10,9 @@ print(players[:4])
 print(players[2:])
 # Print last three players would continue to work as the list of players changes in size
 print(players[-3:])
+
+#Looping through a site
+# Loop through fist three players and print their names
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
