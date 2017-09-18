70c34b5dfd23f4a06b8fe03260690c6fa2e4ffc1
Jiez <jieztee@gmail.com>
2017-09-19 00:00:21 +0800

    5-2 More Conditional Tests

---
+++ CH5_If_Statements/5-2_More_Conditional_Tests.py
@@ -0,0 +1,47 @@
+game = 'Nier'
+print("Is game != 'Horizon'? I predict True")
+print(game != 'Horizon')
+
+print("\nIs game == 'nier'? I predict False")
+print(game == 'nier')
+
+company = "Microsoft"
+print("\nIs company.lower() == 'microsoft'? I predict True")
+print(company.lower() == 'microsoft')
+
+print("\nIs company.lower() == 'MICROSOFT'? I predict False")
+print(company.lower() == 'MICROSOFT')
+
+score = 210
+print("\nIs socre == 210? I predict True")
+print(score == 210)
+
+print("\nIs score != 210? I predict False")
+print(score != 210)
+
+print("\nIs score > 200? I predict True")
+print(score > 200)
+
+print("\nIs score < 200? I predict False")
+print(score < 200)
+
+print("\nIs score >= 209? I predict True")
+print(score >= 209)
+
+print("\nIs score <= 209? I predict False")
+print(score <= 209)
+
+age_1 = 18
+age_2 = 21
+print("\nIs age_1 > 20 and age_2 > 20? I predict False")
+print(age_1 > 20 and age_2 > 20)
+
+print("\nIs age_1 > 20 or age_2 > 20? I predict True")
+print(age_1 > 20 or age_2 > 20)
+
+elements = ['fire', 'water', 'earth']
+print("\nIs 'fire' in elements? I predict True")
+print('fire' in elements)
+
+print("\nIs 'water' not in elements? I predict False")
+print('water' not in elements)
