6dbd828f0361d4252bae75f2b8390ee301003b85
Jiez <jieztee@gmail.com>
2017-09-18 23:28:51 +0800

    5-1 Conditional_Tests.py

---
+++ CH5_If_Statements/5-1_Conditional_Tests.py
@@ -0,0 +1,34 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False")
+print(car == 'audi')
+
+motorcycle = 'suzuki'
+print("\nIs motorcycle == 'suzuki'? I predict True")
+print(car == 'suzuki')
+
+print("\nIs car == 'Suzuki'? I predict False")
+print(car == 'Suzuki')
+
+age = 22
+print("\nIs age == 22? I predict True")
+print(car == 22)
+
+print("\nIs age == '22'? I predict False")
+print(age == '22')
+
+keyword = 'hurricane'
+print("\nIs 'h' in keyword? I predict True")
+print('h' in keyword)
+
+print("\nIs 'm' in keyword? I predict False")
+print('m' in keyword)
+
+phone = 'IPhone'
+print("\nIs phone.lower() == 'iphone'? I predict True")
+print(phone.lower() == 'iphone')
+
+print("\nIs phone.tile() == 'IPhone'? I predict False")
+print(phone.title() == 'IPhone')
