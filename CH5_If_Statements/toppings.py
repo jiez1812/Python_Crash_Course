03803c27c99c364370d9bfc9951cfb0f72f6f92e
Jiez <jieztee@gmail.com>
2018-02-07 00:01:46 +0800

    Checking Special Values

--- CH5_If_Statements/toppings.py
+++ CH5_If_Statements/toppings.py
@@ -6,17 +6,49 @@ if requested_topping != 'anchovies':
 
 # Resetting
 del requested_topping
+print('\n')
 
 # Testing Multiple Conditions
 # Use a series of simple if statements
 
-requested_topping = ['mushrooms', 'extra cheese']
+requested_toppings = ['mushrooms', 'extra cheese']
 
-if 'mushrooms' in requested_topping:
+if 'mushrooms' in requested_toppings:
     print("Adding mushrooms")
-if 'pepperoni' in requested_topping:
+if 'pepperoni' in requested_toppings:
     print("Adding pepperoni")
-if 'extra cheese' in requested_topping:
+if 'extra cheese' in requested_toppings:
     print("Adding extra cheese")
 
 print("\nFinished making your pizza!")
+
+#Resetting
+del requested_toppings
+print('\n')
+
+#Checking for Special Items
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    #Handle situation if running out of green peppers
+    if requested_topping == 'green peppers':
+        print('Sorry, we are out of green peppers right now.')
+    else:
+        print('Adding ' + requested_topping + '.')
+
+print('\nFinished making your pizza')
+
+##Resetting
+del requested_topping
+del requested_toppings
+print('\n')
+
+#Checking that a List is not Empty
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print('Adding ' + requested_topping + '.')
+    print('\nFinished making your pizza.')
+else:
+    print('Are you sure you want a plain pizza?')
