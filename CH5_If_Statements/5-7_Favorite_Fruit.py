4efbab22df629481ece43130a5a56153cd9efd01
Jiez <jieztee@gmail.com>
2018-02-06 23:42:06 +0800

    5-7_Favorite_Fruits

---
+++ CH5_If_Statements/5-7_Favorite_Fruit.py
@@ -0,0 +1,19 @@
+favorite_fruits = ['apple', 'orange', 'banana']
+
+if 'apple' in favorite_fruits:
+    print('You really like apples!')
+
+if 'pineapple' in favorite_fruits:
+    print('You really like pineapples!')
+
+if 'orange' in favorite_fruits:
+    print('You really like oranges!')
+
+if 'rambutan' in favorite_fruits:
+    print('You really like rambutans!')
+
+if 'banana' in favorite_fruits:
+    print('You really like bananas!')
+
+if 'watermelon' in favorite_fruits:
+    print('You really like watermelon!')
