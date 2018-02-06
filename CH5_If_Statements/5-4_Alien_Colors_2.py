0549f4d344da3494e8fa1755ba241ecdaf6daaf3
Jiez <jieztee@gmail.com>
2018-02-06 23:26:11 +0800

    Exercise 5-3 to 5-6

--- CH5_If_Statements/5-4_Alien_Colors_2.py
+++ CH5_If_Statements/5-4_Alien_Colors_2.py
@@ -1,7 +1,9 @@
-alien_color = 'green'
+alien_color = 'red'
 
 if alien_color == 'green':
     print("Congrats! You have earned 5 points.")
 
 if alien_color != 'green':
+    print("Congrats! You have earned 10 points.")
+else:
     pass
---
+++ CH5_If_Statements/5-5_Alien_Colors_3.py
@@ -0,0 +1,8 @@
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("Congrats! You have earned 5 points.")
+elif alien_color == 'yellow':
+    print("Congrats! You have earned 10 points.")
+elif alien_color == 'red':
+    print("Congrats! You have earned 15 points.")
