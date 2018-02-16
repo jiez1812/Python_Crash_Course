2126e71518c5e4f0d55d32a9a843a1729b099890
Jiez <jieztee@gmail.com>
2018-02-17 03:45:44 +0800

    6-3 Glossary

---
+++ CH6_Dictionaries/6-3_Glossary.py
@@ -0,0 +1,15 @@
+glossary = {
+    'Chap_1': 'Introduction',
+    'Chap_2': 'Basics',
+    'Chap_3': 'Intro Lists',
+    'Chap_4': 'Works with Lists',
+    'Chap_5': 'If Statements'
+    }
+
+print('\tChapter\t|\tTitle')
+print('-'*35)
+print("\tChap 1\t|\t" + glossary['Chap_1'] +
+      "\n\tChap 2\t|\t" + glossary['Chap_2'] +
+      "\n\tChap 3\t|\t" + glossary['Chap_3'] +
+      "\n\tChap 4\t|\t" + glossary['Chap_4'] +
+      "\n\tChap 5\t|\t" + glossary['Chap_5'])
