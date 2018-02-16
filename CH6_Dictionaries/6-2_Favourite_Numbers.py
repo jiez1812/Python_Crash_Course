eedce4d9222c37470c50285f6e6157f25593691c
Jiez <jieztee@gmail.com>
2018-02-17 03:19:51 +0800

    6-2 Favorite_Numbers

---
+++ CH6_Dictionaries/6-2_Favourite_Numbers.py
@@ -0,0 +1,8 @@
+fav_nums = {'Joe' : 12, 'Jacq' : 18, 'Moz' : 5, 'Xian' : 20, 'Paw' : 26}
+print("\tName\t|\tNum")
+print("-"*25)
+print("\tJoe\t\t|\t{0}".format(fav_nums['Joe']))
+print("\tJacq\t|\t{0}".format(fav_nums['Jacq']))
+print("\tMoz\t\t|\t{0}".format(fav_nums['Moz']))
+print("\tXian\t|\t{0}".format(fav_nums['Xian']))
+print("\tPaw\t\t|\t{0}".format(fav_nums['Paw']))
