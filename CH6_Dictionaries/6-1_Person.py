3cba61db46c97bca42dcc57d9e8ee552b2e5e915
Jiez <jieztee@gmail.com>
2018-02-17 03:07:29 +0800

    6-1 Person

---
+++ CH6_Dictionaries/6-1_Person.py
@@ -0,0 +1,6 @@
+person = {'first_name' : 'Joe', 'last_name' : 'Kim', 'age' : 24, 'city' : 'Seoul'}
+
+print("First Name\t:\t" + format(person['first_name'] + "\n" +
+      "Last Name\t:\t" + person['last_name'] + "\n"+
+      "Age\t\t\t:\t"+ str(person['age']) +"\n"+
+      "City\t\t:\t" + person['city']) +".")
