3a15a8a9d841cc33a75c808d98c276432e548fb0
Jiez <jieztee@gmail.com>
2018-02-20 02:01:10 +0800

    Looping Through All Values in a Dictionary

---
+++ CH6_Dictionaries/favorite_languages_6.py
@@ -0,0 +1,12 @@
+# Looping through all values in a dictionary
+
+favorite_languages = {
+    'jen' : 'python',
+    'sarah' : 'c',
+    'edward' : 'ruby',
+    'phil' : 'python'
+}
+
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
