18bc633eac777dc0a2e8e259528bebe20ba93419
Jiez <jieztee@gmail.com>
2018-02-20 01:40:56 +0800

    Looping Through All the Keys in a Dictionary

---
+++ CH6_Dictionaries/favorite_languages_3.py
@@ -0,0 +1,10 @@
+# Looping through All the Keys in a Dictionary
+favorite_languages = {
+    'jen' : 'python',
+    'sarah' : 'c',
+    'edward' : 'ruby',
+    'phil' : 'python'
+}
+
+for name in favorite_languages.keys():
+    print(name.title())
