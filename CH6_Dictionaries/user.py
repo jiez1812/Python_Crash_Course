5b501dc61434e5988bc6aed742875065747bbccf
Jiez <jieztee@gmail.com>
2018-02-20 01:32:14 +0800

    Looping through all key-value pairs

---
+++ CH6_Dictionaries/user.py
@@ -0,0 +1,11 @@
+# Looping through All Key-Value Pairs
+
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
