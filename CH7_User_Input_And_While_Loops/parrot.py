36ed404e4890bd5abe7377582fa4dab88a6cd698
Jiez <jieztee@gmail.com>
2018-02-21 12:09:36 +0800

    How the input() Function Works

---
+++ CH7_User_Input_And_While_Loops/parrot.py
@@ -0,0 +1,5 @@
+# input() function pauses your program and waits for the user to enter some text
+# input() function takes one argument: the prompt, or instruction.
+
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
