a1b21dd9f0a28bc40313a069671a6d8bd2469f95
Jiez <jieztee@gmail.com>
2018-02-21 12:35:54 +0800

    Writing Clear Prompt - 1

---
+++ CH7_User_Input_And_While_Loops/greeter_1.py
@@ -0,0 +1,5 @@
+# Each time using input(), you should include a clear, easy-to-follow prompt that tells the user exactly
+# what kind of information you're looking for.
+
+name = input("Please enter your name: ")
+print("Hello, {0}!".format(name))
