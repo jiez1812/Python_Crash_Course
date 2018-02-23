9a7c8f9943a48888437f85cbdd5ec25a35b4d935
Jiez <jieztee@gmail.com>
2018-02-23 12:39:07 +0800

    Default Values - Simplified code

--- CH8_Functions/pets_4.py
+++ CH8_Functions/pets_4.py
@@ -5,4 +5,4 @@ def describe_pet(pet_name, animal_type='dog'):
     print("\nI have a {0}.".format(animal_type))
     print("My {0}'s name is {1}.".format(animal_type, pet_name.title()))
 
-describe_pet(pet_name='willie')
+describe_pet('willie')
