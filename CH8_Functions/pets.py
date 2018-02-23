46e3cfd5dbb653a2c69fb8ee063de7a1d584a4e6
Jiez <jieztee@gmail.com>
2018-02-23 12:30:10 +0800

    Positional Arguments

---
+++ CH8_Functions/pets.py
@@ -0,0 +1,8 @@
+# Positional Arguments
+
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet"""
+    print("\nI have a {0}.".format(animal_type))
+    print("My {0}'s name is {1}.".format(animal_type, pet_name.title()))
+
+describe_pet('hamster', 'harry')
