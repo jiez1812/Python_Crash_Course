0926fcd099ef377e21bbbd2eeac2e4a721eda815
Jiez <jieztee@gmail.com>
2018-02-23 18:21:19 +0800

    Returning a Simple Value

---
+++ CH8_Functions/formatted_name.py
@@ -0,0 +1,9 @@
+# Returning a Simple Value
+
+def get_formatted_name(first_name, last_name):
+    """Returns a full name, neatly formatted"""
+    full_name = first_name + " " + last_name
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
