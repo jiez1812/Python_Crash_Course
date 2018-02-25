4d675bb1e9b5eb580905db53d50f48f668082993
Jiez <jieztee@gmail.com>
2018-02-25 15:23:54 +0800

    Modifying a List in a Function.  - Correct spelling

--- CH8_Functions/printing_models.py
+++ CH8_Functions/printing_models.py
@@ -2,7 +2,7 @@
 
 def print_models(unprinted_designs, completed_models):
     # Simulate printing each design, until none are left.
-    # Move each design to compleated_models after printint.
+    # Move each design to completed_models after printing.
     while unprinted_designs:
         current_design = unprinted_designs.pop()
 
