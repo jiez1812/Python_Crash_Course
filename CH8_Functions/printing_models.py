5491a4f8c867daeb67ce1ea99f2b89e46bf43e46
Jiez <jieztee@gmail.com>
2018-02-25 15:22:40 +0800

    Modifying a List in a Function.  - With Functions  - Unchange Original List

--- CH8_Functions/printing_models.py
+++ CH8_Functions/printing_models.py
@@ -19,5 +19,5 @@ def show_completed_models(completed_models):
 unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 
-print_models(unprinted_designs, completed_models)
+print_models(unprinted_designs[:], completed_models)
 show_completed_models(completed_models)
