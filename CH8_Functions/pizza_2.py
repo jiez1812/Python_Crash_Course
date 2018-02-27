c8d9b5744b4e534b0d76b2414e118eb5909b2c2b
Jiez <jieztee@gmail.com>
2018-02-27 21:42:27 +0800

    Handle Arguments - pizza_2.py

---
+++ CH8_Functions/pizza_2.py
@@ -0,0 +1,10 @@
+# Handle arguments
+
+def make_pizza(*toppings):
+    """Print the list of toppings that have been requested."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print("\t- " + topping)
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
