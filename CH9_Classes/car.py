88bb643fe806615071f2d336120ff6edee7a80fc
Jiez <jieztee@gmail.com>
2018-03-05 21:58:56 +0800

    Incrementing an Attribute's Value Through a Method

--- CH9_Classes/car.py
+++ CH9_Classes/car.py
@@ -29,11 +29,23 @@ class Car():
         else:
             print("You can't roll back an odometer.")
 
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 
 # Modifying Attribute Values
 #my_new_car.odometer_reading = 23
 my_new_car.update_odometer(23)
-
 my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2013)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
