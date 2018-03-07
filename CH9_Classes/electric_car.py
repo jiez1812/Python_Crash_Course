27f4f2cf6ea89ea69945296f01804345d40787aa
Jiez <jieztee@gmail.com>
2018-03-07 21:53:37 +0800

    Inheritance - Overriding Methods from the Parent Class & Instances as Attributes

--- CH9_Classes/electric_car.py
+++ CH9_Classes/electric_car.py
@@ -33,18 +33,29 @@ class Car():
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
 
-class ElectricCar(Car):
-    """Represent aspects of a car, specific to electric vehicles."""
-    def __int__(self, make, model, year):
-        """Initialize attributes of the parent class.
-        Then initialize attributes specific to an electric car."""
-        super().__init__(make, model, year)
-        self.battery_size = 70
+class Battery():
+    """A simple attempt to model a battery for an electic car."""
+
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
 
     def describe_battery(self):
         """Print a statement describing the battery size."""
-        print("This car has a {}-kWh battery.".format(self.battery_size))
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        """Electric cars don't have a gas tanks"""
+        print("This car doesn't need a gas tank.")
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
