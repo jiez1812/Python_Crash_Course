5581c84724f7ec45afb0495faf4146972cc73c70
Jiez <jieztee@gmail.com>
2018-03-08 23:08:27 +0800

    Importing Module into a Module

--- CH9_Classes/electric_car.py
+++ CH9_Classes/electric_car.py
@@ -1,37 +1,4 @@
-# Inheritance
-
-class Car():
-    """A simple attempt to represent a car."""
-
-    def __init__(self, make, model, year):
-        """Initialize attributes to describe a car."""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        """Return a neatly formatted descriptive name."""
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return  long_name.title()
-
-    def read_odometer(self):
-        """Print a statement showing the car's mileage"""
-        print("This car has " + str(self.odometer_reading) + " miles on it.")
-
-    def update_odometer(self, mileage):
-        """
-        Set the odometer reading to the given value
-        Reject the change if it attempts to roll the odometer back
-        """
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer.")
-
-    def increment_odometer(self, miles):
-        """Add the given amount to the odometer reading."""
-        self.odometer_reading += miles
+from car import Car
 
 class Battery():
     """A simple attempt to model a battery for an electic car."""
@@ -66,8 +33,3 @@ class ElectricCar(Car):
     def fill_gas_tank(self):
         """Electric cars don't have a gas tanks"""
         print("This car doesn't need a gas tank.")
-
-my_tesla = ElectricCar('tesla', 'model s', 2016)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
