a92ce8d3ab62a9ae1999c1e8224f1dfd6f2a6cd7
Jiez <jieztee@gmail.com>
2018-03-08 22:48:48 +0800

    Storing Multiple Classes in a Module

--- CH9_Classes/car.py
+++ CH9_Classes/car.py
@@ -32,3 +32,37 @@ class Car():
     def increment_odometer(self, miles):
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
+
+class Battery():
+    """A simple attempt to model a battery for an electic car."""
+
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge"
+        print(message)
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        """Electric cars don't have a gas tanks"""
+        print("This car doesn't need a gas tank.")
