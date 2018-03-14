655ca78aa49bcae673cf0d5a595fcdca7cee10dd
Jiez <jieztee@gmail.com>
2018-03-14 22:53:22 +0800

    Analying Text

--- CH10_Files_and_Exception/alice.py
+++ CH10_Files_and_Exception/alice.py
@@ -6,3 +6,8 @@ try:
 except FileNotFoundError:
     message = "Sorry, the file " + filename + " does not exist."
     print(message)
+else:
+    # Count the approximate number of words in the file
+    words = contents.split()
+    num_words = len(words)
+    print("The file " + filename + " has about " + str(num_words) + " words.")
