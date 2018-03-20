17331359a4eb043fcaf629b36f759dc310f2c6ef
Jiez <jieztee@gmail.com>
2018-03-20 23:38:35 +0800

    Storing Data - Refactoring

--- CH10_Files_and_Exception/remember_me.py
+++ CH10_Files_and_Exception/remember_me.py
@@ -1,16 +1,26 @@
 import json
 
-# Load the username, if it has been stored previously
-# Otherwise, prompt for the username and store it.
+def get_stored_username():
+    """Get stored username if available"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
-filename = 'username.json'
-try:
-    with open(filename) as f_obj:
-        username = json.load(f_obj)
-except FileNotFoundError:
-    username = input("What is your name?")
-    with open(filename, 'w') as f_obj:
-        json.dump(username, f_obj)
-        print("We'll remember you when you come back, " + username + "!")
-else:
-    print("Welcome back, " + username + "!")
+def greet_user():
+    """Greet the user by name"""
+    username = get_stored_username()
+    if username:
+        print("Welcome back, " + username + "!")
+    else:
+        username = input("What is your name?")
+        filename = 'username.json'
+        with open(filename, 'w') as f_obj:
+            json.dump(username, f_obj)
+            print("We'll remember you when you come back, " + username + "!")
+
+greet_user()
